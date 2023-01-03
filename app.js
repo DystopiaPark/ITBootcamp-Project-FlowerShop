51a2133ef267e97ccb3dce67580666f033eb8ebd
Filip <68464364+DystopiaPark@users.noreply.github.com>
2023-01-03 19:04:05 +0100

    fixed issue with img resolution

--- app.js
+++ app.js
@@ -83,9 +83,9 @@ izracunajBtn.addEventListener("click", () => {
   let gerberiCena = gerberiInput.value * 70;
   let cenaZaSve = ruzeCena + ljiljaniCena + gerberiCena;
   let cenaPopust = 0;
-  let ruzaImgLink = "/slike/rose.png";
-  let ljiljanImgLink = "/slike/lily.png";
-  let gerberImgLink = "/slike/gerber.png";
+  let ruzaImgLink = "/slike/rose1.png";
+  let ljiljanImgLink = "/slike/lily1.png";
+  let gerberImgLink = "/slike/gerber1.png";
 
   imgAdd(ruzeInput, ruzaImgLink, ruzaDiv);
   imgAdd(ljiljaniInput, ljiljanImgLink, ljiljanDiv);
